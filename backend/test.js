93fcec86d4984090059376b25d30289bfe05fd80
Zhenyi Li <zhenyili@Zhenyis-MacBook-Pro.local>
2020-07-10 14:35:37 -0400

    new update

--- backend/test.js
+++ backend/test.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const promiseFunc = () => {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.readFile("./readFrom.txt", (err, source) => {
       if (err) {
         reject(err);
@@ -11,9 +11,49 @@ const promiseFunc = () => {
       }
     });
   });
+};
+
+const promiseFuncPlusOne = (content) => {
+  const promise = new Promise((resolve, reject) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(content.concat("1"));
+    }
+  });
   return promise;
 };
-    
-// promiseFunc().then((content) => console.log(content));
 
+const p0 = new Promise((resolve, reject) => {
+  fs.readFile("./readFrom.txt", (err, source) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(source.toString());
+    }
+  });
+});
+
+promiseFunc()
+  .then((content) => {
+    console.log(content);
+    return content;
+  })
+  .then((content) => console.log(content));
+
+let p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 3 * 100);
+});
+
+p.then((result) => {
+  console.log(result);
+  return result * 2;
+})
+  .then((result) => {
+    console.log(result);
+    return result;
+  })
+  .then((result) => console.log(result));
 console.log(path.join(__dirname, "../anything"));
